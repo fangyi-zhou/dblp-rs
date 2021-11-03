3c67ecadd68819644462b6bffca681b58775ec45
Fangyi Zhou <me@fangyi.io>
2021-11-03 23:41:34 +0000

    Add some rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,35 @@ async fn make_request(api_endpoint: &str, query_string: &str) -> anyhow::Result<
     Ok(result)
 }
 
+/// Search for a publication, returns a JSON value
+/// ```
+/// # async fn publication() -> anyhow::Result<()> {
+/// use dblp_rs::search_publication;
+/// let result = search_publication("The Part-Time Parliament").await;
+/// # Ok(()) }
+/// ```
 pub async fn search_publication(query_string: &str) -> anyhow::Result<serde_json::Value> {
     make_request(PUBLICATION_API_ENDPOINT, query_string).await
 }
 
+/// Search for an author, returns a JSON value
+/// ```
+/// # async fn author() -> anyhow::Result<()> {
+/// use dblp_rs::search_author;
+/// let result = search_author("Leslie Lamport").await;
+/// # Ok(()) }
+/// ```
 pub async fn search_author(query_string: &str) -> anyhow::Result<serde_json::Value> {
     make_request(AUTHOR_API_ENDPOINT, query_string).await
 }
 
+/// Search for a venue, returns a JSON value
+/// ```
+/// # async fn venue() -> anyhow::Result<()> {
+/// use dblp_rs::search_venue;
+/// let result = search_venue("TOCS").await;
+/// # Ok(()) }
+/// ```
 pub async fn search_venue(query_string: &str) -> anyhow::Result<serde_json::Value> {
     make_request(VENUE_API_ENDPOINT, query_string).await
 }
