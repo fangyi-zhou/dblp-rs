34bc262f3e297423cd44a8d5214b7beb17aa8c1f
Fangyi Zhou <me@fangyi.io>
2021-11-03 23:27:48 +0000

    Add a first coarse version

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,55 @@
+const PUBLICATION_API_ENDPOINT: &str = "https://dblp.org/search/publ/api";
+const AUTHOR_API_ENDPOINT: &str = "https://dblp.org/search/author/api";
+const VENUE_API_ENDPOINT: &str = "https://dblp.org/search/venue/api";
+
+extern crate anyhow;
+extern crate reqwest;
+extern crate serde_json;
+
+async fn make_request(api_endpoint: &str, query_string: &str) -> anyhow::Result<serde_json::Value> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(api_endpoint)
+        .query(&[("q", query_string), ("format", "json")])
+        .send()
+        .await?;
+    let result = response.json::<serde_json::Value>().await?;
+    //println!("{:?}", result);
+    Ok(result)
+}
+
+pub async fn search_publication(query_string: &str) -> anyhow::Result<serde_json::Value> {
+    make_request(PUBLICATION_API_ENDPOINT, query_string).await
+}
+
+pub async fn search_author(query_string: &str) -> anyhow::Result<serde_json::Value> {
+    make_request(AUTHOR_API_ENDPOINT, query_string).await
+}
+
+pub async fn search_venue(query_string: &str) -> anyhow::Result<serde_json::Value> {
+    make_request(VENUE_API_ENDPOINT, query_string).await
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    extern crate tokio;
+    use super::*;
+
+    #[tokio::test]
+    async fn integration_test_publicaion() {
+        let result = search_publication("The Part-Time Parliament").await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn integration_test_author() {
+        let result = search_author("Leslie Lamport").await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn integration_test_venue() {
+        let result = search_venue("TOCS").await;
+        assert!(result.is_ok());
     }
 }
